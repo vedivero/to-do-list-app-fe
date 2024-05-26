5474a6bb8239640f9269e9bcfed91802c66b5e16
vedivero <vedivero@github.com>
2024-05-26 20:08:16 +0900

    기능 롤백 완료

--- src/App.js
+++ src/App.js
@@ -3,42 +3,62 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import TodoBoard from "./components/TodoBoard";
-
+import api from "./utils/api";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import api from "./utils/api";
+
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [todoValue, setTodoValue] = useState("");
+
   const getTasks = async () => {
     const response = await api.get("/tasks");
-    console.log("rrrrrr", response);
     setTodoList(response.data.data);
   };
-  const addTask = async () => {
+  useEffect(() => {
+    getTasks();
+  }, []);
+  const addTodo = async () => {
     try {
       const response = await api.post("/tasks", {
         task: todoValue,
         isComplete: false,
       });
       if (response.status === 200) {
-        console.log("성공");
-        //1. 입력한 값이 안사라짐
-        setTodoValue("");
-        //2. 추가한 값이 안보임
         getTasks();
-      } else {
-        throw new Error("task can not be added");
       }
-    } catch (err) {
-      console.log("error", err);
+      setTodoValue("");
+    } catch (error) {
+      console.log("error:", error);
     }
   };
 
-  useEffect(() => {
-    getTasks();
-  }, []);
+  const deleteItem = async (id) => {
+    try {
+      console.log(id);
+      const response = await api.delete(`/tasks/${id}`);
+      if (response.status === 200) {
+        getTasks();
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const toggleComplete = async (id) => {
+    try {
+      const task = todoList.find((item) => item._id === id);
+      const response = await api.put(`/tasks/${id}`, {
+        isComplete: !task.isComplete,
+      });
+      if (response.status === 200) {
+        getTasks();
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <Container>
       <Row className="add-item-row">
@@ -46,19 +66,23 @@ function App() {
           <input
             type="text"
             placeholder="할일을 입력하세요"
+            onChange={(event) => setTodoValue(event.target.value)}
             className="input-box"
             value={todoValue}
-            onChange={(event) => setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTask}>
+          <button onClick={addTodo} className="button-add">
             추가
           </button>
         </Col>
       </Row>
 
-      <TodoBoard todoList={todoList} />
+      <TodoBoard
+        todoList={todoList}
+        deleteItem={deleteItem}
+        toggleComplete={toggleComplete}
+      />
     </Container>
   );
 }
--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,16 +1,19 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodoBoard = ({ todoList }) => {
+const TodoBoard = ({ todoList, deleteItem, toggleComplete }) => {
   return (
     <div>
       <h2>Todo List</h2>
-      {todoList.length > 0 ? (
-        todoList.map((item) => <TodoItem item={item} />)
-      ) : (
-        <h2>There is no Item to show</h2>
-      )}
-      {/* <TodoItem/> will be here once we get the todoList */}
+      {todoList.length > 0 &&
+        todoList.map((item, index) => (
+          <TodoItem
+            item={item}
+            key={index}
+            deleteItem={deleteItem}
+            toggleComplete={toggleComplete}
+          />
+        ))}
     </div>
   );
 };
